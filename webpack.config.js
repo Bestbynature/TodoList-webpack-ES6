1ed979d2a0897566b8a3ee0a2e4c721765d873dd
Prince Dammy <bestbynature2010@gmail.com>
2023-03-29 15:31:44 +0100

    Update the webpack file to reflect the new entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/functionality.js',
   //  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
